a5519264aba6c9a784bf65be889d00afa8a04d8c
rezha4 <rezha.bahari@gmail.com>
2024-09-27 07:15:25 +0700

    feat: tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@prisma/nuxt"],
+  modules: ["@prisma/nuxt", "@nuxtjs/tailwindcss"],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   experimental: {
