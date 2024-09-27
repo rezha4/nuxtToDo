0fecdfd5aeda97617051a4596384c201b333f9d2
rezha4 <rezha.bahari@gmail.com>
2024-09-27 07:12:41 +0700

    CRUD handler

---
+++ server/api/todos.ts
@@ -0,0 +1,34 @@
+import prisma from "~/lib/prisma";
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const id = query.id ? Number(query.id) : null;
+
+  if (event.method === "GET") {
+    return await prisma.todo.findMany();
+  }
+
+  if (event.method === "POST") {
+    const title = await readBody(event);
+    return await prisma.todo.create({
+      data: {
+        title: title,
+        done: false,
+      },
+    });
+  }
+
+  if (event.method === "DELETE" && id) {
+    return await prisma.todo.delete({ where: { id } });
+  }
+
+  if (event.method === "PUT" && id) {
+    const title = await readBody(event);
+    return await prisma.todo.update({
+      where: { id },
+      data: {
+        title: title,
+      },
+    });
+  }
+});
